acce153b69a77887387f56de0eec453ef8350819
JUNU <wnsdn2186@naver.com>
2022-06-07 14:41:50 +0900

    GoogleMapExample

---
+++ GoogleMapExample/app/src/main/java/com/example/googlemap/MainActivity.kt
@@ -0,0 +1,277 @@
+package com.example.googlemap
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
+import java.io.IOException
+import java.util.*
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private val UPDATE_INTERVAL = 1000 // 1초
+    private val FASTEST_UPDATE_INTERVAL = 500 // 0.5초
+
+    private var mMap: GoogleMap? = null
+    private var currentMarker: Marker? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var mCurrentLocation: Location
+    private lateinit var currentPosition: LatLng
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = UPDATE_INTERVAL.toLong()
+            smallestDisplacement = 10.0f
+            fastestInterval = FASTEST_UPDATE_INTERVAL.toLong()
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(locationRequest)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+        //퍼미션 요청 대화상자 (권한이 없을때) & 실행 시 초기 위치를 서울 중심부로 이동
+        setDefaultLocation()
+
+        if (checkPermission()) { // 1. 위치 퍼미션을 가지고 있는지 확인
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식)
+            startLocationUpdates() // 3. 위치 업데이트 시작
+        } else {  //2. 권한이 없다면
+            // 3-1. 사용자가 권한이 없는 경우에는
+            val permissionListener = object : PermissionListener {
+                // 권한 얻기에 성공했을 때 동작 처리
+                override fun onPermissionGranted() {
+                    startLocationUpdates()
+                }
+
+                // 권한 얻기에 실패했을 때 동작 처리
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "위치 권한이 거부되었습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("[설정] 에서 위치 접근 권한을 부여해야만 사용이 가능합니다.")
+                .setPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                .check()
+        }
+    }
+
+    //위치정보 요청시 호출
+    var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val locationList = locationResult.locations
+            if (locationList.size > 0) {
+                val location = locationList[locationList.size - 1]
+                currentPosition = LatLng(location.latitude, location.longitude)
+                val markerTitle: String = getCurrentAddress(currentPosition)
+                val markerSnippet =
+                    "위도: ${location.latitude.toString()}, 경도: ${location.longitude}"
+
+                //현재 위치에 마커 생성하고 이동
+                setCurrentLocation(location, markerTitle, markerSnippet)
+                mCurrentLocation = location
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        // 위치서비스 활성화 여부 check
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting()
+        } else {
+            if (checkPermission()) {
+                mFusedLocationClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.myLooper()!!
+                )
+                if (mMap != null) mMap!!.isMyLocationEnabled = true
+                //2. 지도에 +.- 버튼 보이기.
+                if (mMap != null) mMap!!.uiSettings.isZoomControlsEnabled = true
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (checkPermission()) {
+            startLocationUpdates()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mFusedLocationClient.removeLocationUpdates(locationCallback)
+
+    }
+
+    fun getCurrentAddress(latlng: LatLng): String {
+        //지오코더: GPS를 주소로 변환
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address>?
+        try {
+            addresses = geocoder.getFromLocation(
+                latlng.latitude,
+                latlng.longitude,
+                1
+            )
+        } catch (ioException: IOException) {
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show()
+            return "지오코더 사용불가"
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show()
+            return "잘못된 GPS 좌표"
+        }
+
+        return if (addresses == null || addresses.isEmpty()) {
+            Toast.makeText(this, "주소 발견 불가", Toast.LENGTH_LONG).show()
+            "주소 발견 불가"
+        } else {
+            val address = addresses[0]
+            address.getAddressLine(0).toString()
+        }
+    }
+
+    private fun checkLocationServicesStatus(): Boolean {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+    }
+
+    fun setCurrentLocation(location: Location, markerTitle: String?, markerSnippet: String?) {
+        currentMarker?.remove()
+
+        val currentLatLng = LatLng(location.latitude, location.longitude)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(currentLatLng)
+        markerOptions.title(markerTitle)
+        markerOptions.snippet(markerSnippet)
+        markerOptions.draggable(true)
+
+        currentMarker = mMap!!.addMarker(markerOptions)
+
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f)
+        mMap!!.animateCamera(cameraUpdate)
+    }
+
+
+    private fun setDefaultLocation() {
+        //이전위치값 가져오기
+        var lastLocation: Location
+        if (checkPermission()) {
+            mFusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) lastLocation = location
+            }
+        }
+
+        //초기 위치를 서울로
+        val DEFAULT_LOCATION = LatLng(37.56, 126.97)
+        val markerTitle = "위치정보 가져올 수 없음"
+        val markerSnippet = "위치 퍼미션과 GPS 활성 여부 확인 필요"
+
+        var location = Location("")
+        location.latitude = DEFAULT_LOCATION.latitude
+        location.longitude = DEFAULT_LOCATION.longitude
+        setCurrentLocation(location, markerTitle, markerSnippet)
+    }
+
+    private fun checkPermission(): Boolean {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        return hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    /******** 위치서비스 활성화 여부 check *********/
+    private val GPS_ENABLE_REQUEST_CODE = 2001
+    private var needRequest = false
+
+    private fun showDialogForLocationServiceSetting() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage(
+            "앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+        )
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정") { _, _ ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE)
+        }
+        builder.setNegativeButton(
+            "취소"
+        ) { dialog, _ -> dialog.cancel() }
+        builder.create().show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_ENABLE_REQUEST_CODE ->
+                //사용자가 GPS를 켰는지 검사함
+                if (checkLocationServicesStatus()) {
+                    needRequest = true
+                    return
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "위치 서비스가 꺼져 있어, 현재 위치를 확인할 수 없습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
+    }
+}
